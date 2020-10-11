65cc1bbd961c2b8f1c89e87836c9798e4dabf415
deve-w <72653887+deve-w@users.noreply.github.com>
2020-10-11 16:39:20 +0800

    Add files via upload

---
+++ Q8.py
@@ -0,0 +1,6 @@
+#Write a program that accepts a comma separated sequence of words as input
+# and prints the words in a comma-separated sequence after sorting them alphabetically.
+
+lst = input().split(",")
+lst.sort()
+print(",".join(lst))
